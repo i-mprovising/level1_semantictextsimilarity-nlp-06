f3e93d3f55ba5fab1cc37aa41c960aa7a59efb07
Kim Gibeom <gibum1228@gmail.com>
2023-04-14 00:13:42 +0000

    init

--- utils/utils.py
+++ utils/utils.py
@@ -29,8 +29,8 @@ def set_seed(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    cudnn.benchmark = False
-    cudnn.deterministic = True
+    # torch.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
+    # torch.cuda.manual_seed_all(seed)
+    # cudnn.benchmark = False
+    # cudnn.deterministic = True
