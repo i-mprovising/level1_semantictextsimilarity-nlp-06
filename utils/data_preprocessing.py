8e7bbec5096e775e4681e77885c590cb16b34a33
ella0106 <ella0106@naver.com>
2023-04-14 19:04:32 +0000

    add eda func

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -5,6 +5,7 @@ from tqdm.auto import tqdm
 # spellceck
 from pykospacing import Spacing
 from symspellpy_ko import KoSymSpell, Verbosity
+from koeda import SR, RI, RS, RD
 
 def remove_special_word(df):
     df['sentence_1'] = df['sentence_1'].str.replace(pat=r'[^\w]', repl=r' ', regex=True)
@@ -70,28 +71,114 @@ def spellcheck(df):
 
     return checked_df
 
-def synonym_replacement(df, n):
+def sr(df):
     """
     문장 내 단어를 유의어로 바꿔주는 함수입니다.
 
     필요 라이브러리 :
-    pip install konlpy
+    pip install koeda
 
     Args:
-        df (pd.DataFrame): 원본 train data
-        n (int) : replacement를 할 단어의 개수
+        df (pd.DataFrame): 원본 data
 
     Returns:
         pd.DataFrame    
     """
-    
+    sr_df = df.copy()
+    sen1 = df['sentence_1'].to_list()
+    sen2 = df['sentence_2'].to_list()
 
-    return
+    func = SR("Okt")
+    sen1 = func(sen1, 0.3)
+    sen2 = func(sen2, 0.3)
+
+    sr_df['sentence_1'] = sen1
+    sr_df['sentence_2'] = sen2    
+
+    return sr_df
+
+def ri(df):
+    """
+    문장 내에 유의어를 무작위로 삽입하는 함수입니다.
+
+    필요 라이브러리 :
+    pip install koeda
+
+    Args:
+        df (pd.DataFrame): 원본 data
+
+    Returns:
+        pd.DataFrame    
+    """
+    ri_df = df.copy()
+    sen1 = df['sentence_1'].to_list()
+    sen2 = df['sentence_2'].to_list()
+
+    func = RI("Okt")
+    sen1 = func(sen1, 0.3)
+    sen2 = func(sen2, 0.3)
+
+    ri_df['sentence_1'] = sen1
+    ri_df['sentence_2'] = sen2    
+
+    return ri_df
+
+def rs(df):
+    """
+    문장 내 단어들의 위치를 바꾸는 함수입니다.
+
+    필요 라이브러리 :
+    pip install koeda
+
+    Args:
+        df (pd.DataFrame): 원본 data
+
+    Returns:
+        pd.DataFrame    
+    """
+    rs_df = df.copy()
+    sen1 = df['sentence_1'].to_list()
+    sen2 = df['sentence_2'].to_list()
+
+    func = RS("Okt")
+    sen1 = func(sen1, 0.3)
+    sen2 = func(sen2, 0.3)
+
+    rs_df['sentence_1'] = sen1
+    rs_df['sentence_2'] = sen2    
+
+    return rs_df
+
+def rd(df):
+    """
+    문장 내 단어를 무작위로 삭제하는 함수입니다.
+
+    필요 라이브러리 :
+    pip install koeda
+
+    Args:
+        df (pd.DataFrame): 원본 data
+
+    Returns:
+        pd.DataFrame    
+    """
+    rd_df = df.copy()
+    sen1 = df['sentence_1'].to_list()
+    sen2 = df['sentence_2'].to_list()
+
+    func = RD("Okt")
+    sen1 = func(sen1, 0.3)
+    sen2 = func(sen2, 0.3)
+
+    rd_df['sentence_1'] = sen1
+    rd_df['sentence_2'] = sen2    
+
+    return rd_df
 
 # 전처리 코드 테스트
 if __name__ == "__main__":
     train_df, _, _ = utils.get_data()
-    preprocessed_df = swap_sentence(train_df)
+    preprocessed_df = basic_eda(train_df, "RD", 0.5)
 
     print('-'*30)
     print("전처리 전", train_df.head(5), sep='\n')
