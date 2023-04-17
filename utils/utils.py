d99ec8094060fe2922888ec7ff9bc7f02ebab5fd
Kim Gibeom <gibum1228@gmail.com>
2023-04-17 14:55:13 +0000

    load best(last) check point model

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,5 @@
 import os
 import random
-import torch
 import numpy as np
 import pandas as pd
 import torch.backends.cudnn as cudnn
@@ -8,6 +7,7 @@ import torch.backends.cudnn as cudnn
 from datetime import datetime
 from datetime import timezone
 from datetime import timedelta
+from glob import glob
 
 
 def get_folder_name(CFG):
@@ -26,3 +26,19 @@ def get_data():
     test_df = pd.read_csv('./data/test.csv')
 
     return train_df, val_df, test_df
+
+
+def get_best_check_point(save_path):
+    """
+    가장 최근 체크포인트로 학습된 모델을 가져오는 메소드
+    """
+    check_point_list = glob(f'results/{save_path}/*/*/*/epoch*')
+    check_point_list.sort(reverse=True)
+    
+    last_check_point = check_point_list[0]
+    
+    return last_check_point
+
+
+if __name__ == "__main__":
+    get_best_check_point('2023-04-17-14:13:02_KGB')
