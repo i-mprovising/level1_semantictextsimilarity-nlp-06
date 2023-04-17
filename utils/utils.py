a8bde95bc72c151779be6356de148ef4d0941559
Kim Gibeom <gibum1228@gmail.com>
2023-04-17 22:05:36 +0000

    solve error

--- utils/utils.py
+++ utils/utils.py
@@ -32,7 +32,7 @@ def get_best_check_point(save_path):
     """
     가장 최근 체크포인트로 학습된 모델을 가져오는 메소드
     """
-    check_point_list = glob(f'results/{save_path}/*/*/*/epoch*')
+    check_point_list = glob(f'{save_path}/*/*/*/epoch*')
     check_point_list.sort(reverse=True)
     
     last_check_point = check_point_list[0]
